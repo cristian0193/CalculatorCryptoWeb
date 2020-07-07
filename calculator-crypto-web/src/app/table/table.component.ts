7806697135c5e0d3e614fba2e8b227d075458824
Christian Rodriguez <cristian010193@gmail.com>
2020-07-06 19:25:12 -0500

    - Se corrigue problema de calculos instantaneos ingresados por input

--- calculator-crypto-web/src/app/table/table.component.ts
+++ calculator-crypto-web/src/app/table/table.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,DoCheck } from '@angular/core';
 import * as $ from 'jquery';
 
 @Component({
@@ -6,24 +6,48 @@ import * as $ from 'jquery';
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
-export class TableComponent {
+export class TableComponent implements DoCheck{
 
+  public precioDolar: number;
+  public cantidad: number;
+  public resultado: number;
+  public totalCalculo:number;
 
-  getCalculation(){ 
-    $("#table-coin tbody tr").on("click", function(event){
-      var priceDolar = parseFloat($(this).find("#priceDolar").html());
-      var quantity = $(this).find('input[type="number"]').val();
-      console.log(priceDolar);
-      console.log(quantity);
-      
-      
-      if (typeof quantity === "string") {
-       var resultDolar = priceDolar * parseFloat(quantity);
-       console.log(resultDolar);
-       
-      }
-      event.stopImmediatePropagation();
-    });
+  constructor(){
+    this.precioDolar = 4000;
+    this.cantidad= 0;
   }
 
+  public items: Array<any> = [
+    {
+       nombre: 'BItcoin',
+       precio: 9000,
+       precioCol: 36000000,
+       cantidad: 0,
+       totalUSD: 0,
+       totalCOL: 0,
+       resultadoTotal:0
+    },
+    {
+      nombre: 'Ether',
+      precio: 250,
+      precioCol: 850000,
+      cantidad: 0,
+      totalUSD: 0,
+      totalCOL: 0,
+      resultadoTotal:0 
+   }
+];
+
+ngDoCheck(){
+  let suma = 0;
+  for (let index = 0; index < this.items.length; index++) {
+   suma = suma + this.items[index].resultadoTotal;
+  }
+  this.totalCalculo = suma;
+}
+
+getCalculation(cantidad:number, precio:number, index:number){ 
+    this.items[index].resultadoTotal = cantidad*precio;
+}
 }
